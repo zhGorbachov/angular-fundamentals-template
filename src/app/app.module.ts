823e2f8f41a740899409481e12070fcf8f005b0d
Yevhenii Horbachov <yevhenii_horbachov@epam.com>
2025-04-22 14:10:31 +0300

    Task has done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MockDataService } from '@app/mock-data.service';
 import { AppLoader } from '@app/loader/loader.component';
+import {CoursesModule} from "@features/courses/courses.module";
+import {CoursesListModule} from "@features/courses/courses-list/courses-list.module";
+import {CourseInfoModule} from "@features/course-info/course-info.module";
 
 @NgModule({
   declarations: [
     AppComponent, AppLoader
   ],
   imports: [
-    BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule,
+    BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule, CoursesModule, CoursesListModule, CourseInfoModule
   ],
   providers: [MockDataService],
   bootstrap: [AppComponent]
---
+++ src/app/features/course-info/course-info.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CourseInfoComponent } from './course-info.component';
+
+describe('CourseInfoComponent', () => {
+  let component: CourseInfoComponent;
+  let fixture: ComponentFixture<CourseInfoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CourseInfoComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CourseInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
